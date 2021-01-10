d3c9b869f49d74bc1cde14521345f53a7b52a060
Nikki G <nikki.goodridge@gmail.com>
2021-01-10 16:47:42 -0500

    comma problem fixed

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,7 @@ const includeNumbersElement = document.getElementById('includeNumbers')
 const includeSymbolsElement = document.getElementById ('includeSymbols')
 const password = document.getElementById('password')
 
+//array assignments
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBERS_CHAR_CODES = arrayFromLowToHigh(48, 57)
@@ -14,9 +15,11 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(5
 ).concat(arrayFromLowToHigh(123, 126)
 )
 
+//allows the sliding scale/# box to work for getting password length
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
+//generates the specific password
 generate.addEventListener ('click', e => {
   e.preventDefault()
 const characterAmount = characterAmountNumber.value
@@ -28,7 +31,7 @@ const password = generatePassword(characterAmount, includeUppercase, includeNumb
 })
 
 
-
+//generates password characters
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols){
   let charCodes = LOWERCASE_CHAR_CODES
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
@@ -42,9 +45,10 @@ function generatePassword(characterAmount, includeUppercase, includeNumbers, inc
     passwordCharacters.push (String.fromCharCode(characterCode))
   }
   
-  return password.innerText= passwordCharacters
+  return password.innerText= passwordCharacters.join(""); //join takes away the commas from the array
 }
 
+//for loop that creates the arrays
 function arrayFromLowToHigh(low, high) {
   const array = []
   for (i= low; i <= high; i++) {
@@ -53,7 +57,7 @@ function arrayFromLowToHigh(low, high) {
   return array
 }
 
-//make sliding scale work
+//make sliding scale work to show numbers in box
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
